e1554d99263fe99a38ede5d7bb6d9aa65db87ece
Josh Steiner <josh@jsteiner.me>
2015-12-15 20:53:06 -0500

    Advent of Code: Day 5

---
+++ adventofcode/day5/nice_strings.hs
@@ -0,0 +1,31 @@
+import System.IO
+import Data.List (isInfixOf, group)
+
+main :: IO ()
+main =  do
+  fromHandle <- openFile "input" ReadMode
+  contents   <- hGetContents fromHandle
+  putStr . show . length . niceStrings . lines $ contents
+
+niceStrings :: [String] -> [String]
+niceStrings = filter niceString
+
+niceString :: String -> Bool
+niceString s = and [ threeVowels s
+                   , duplicateChain s
+                   , not $ hasNaughtyLetters s
+                   ]
+
+threeVowels :: String -> Bool
+threeVowels = (>2) . length . filter isVowel
+
+isVowel :: Char -> Bool
+isVowel c = c `elem` "aeiou"
+
+duplicateChain :: String -> Bool
+duplicateChain = any ((>1) . length) . group
+
+hasNaughtyLetters :: String -> Bool
+hasNaughtyLetters s = any (`isInfixOf` s) naughtyLetters
+
+naughtyLetters = ["ab", "cd", "pq", "xy"]
